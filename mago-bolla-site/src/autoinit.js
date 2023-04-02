040882828b9cb0680159a59ca940e73868f5dc7c
Ivan Toselli <ivan.toselli31@gmail.com>
2023-04-02 13:10:59 +0200

    SiteU Update 001b

---
+++ mago-bolla-site/src/autoinit.js
@@ -0,0 +1,9 @@
+import M from 'materialize-css'
+
+export default {
+        mounted () {
+        document.addEventListener('DOMContentLoaded', function() {
+            M.AutoInit();
+        });
+    }
+}
--- mago-bolla-site/src/main.js
+++ mago-bolla-site/src/main.js
@@ -8,6 +8,7 @@ import router from './router'
 import './assets/main.css'
 import '../node_modules/materialize-css/dist/css/materialize.min.css'
 //import '../node_modules/material-design-icons/iconfont/material-icons.css'
+import './autoinit.js'
 
 const app = createApp(App)
 
